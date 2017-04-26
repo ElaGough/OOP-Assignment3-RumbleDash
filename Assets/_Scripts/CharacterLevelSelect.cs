a75f10308133d1e296509d2f5641b5a77b3ece2c
Ela Gough <ela.gough@gmail.com>
2017-04-26 04:15:46 +0100

    character screen adding 2 more

--- Assets/_Scripts/CharacterLevelSelect.cs
+++ Assets/_Scripts/CharacterLevelSelect.cs
@@ -36,36 +36,62 @@ public class CharacterLevelSelect : MonoBehaviour {
 	public static bool other2bool;
 	public static bool otherr2bool;
 
-	//private GameObject b1Text = GameObject.Find ("b1Text");
-
 	void Update()
 	{
 		if (buddy1bool == true) {
 			b1Text.SetActive(true);
 			s1Text.SetActive(false);
-			/*or1Text.SetActive(false);
-			orr1Text.SetActive(false);*/
+			or1Text.SetActive(false);
+			orr1Text.SetActive(false);
 		}
 
 		if (stela1bool == true) {
 			b1Text.SetActive(false);
 			s1Text.SetActive(true);
-			/*or1Text.SetActive(false);
-			orr1Text.SetActive(false);*/
+			or1Text.SetActive(false);
+			orr1Text.SetActive(false);
+		}
+
+		if (other1bool == true) {
+			b1Text.SetActive(false);
+			s1Text.SetActive(false);
+			or1Text.SetActive(true);
+			orr1Text.SetActive(false);
+		}
+
+		if (otherr1bool == true) {
+			b1Text.SetActive(false);
+			s1Text.SetActive(false);
+			or1Text.SetActive(false);
+			orr1Text.SetActive(true);
 		}
 
 		if (buddy2bool == true) {
 			b2Text.SetActive(true);
 			s2Text.SetActive(false);
-			/*or2Text.SetActive(false);
-			orr2Text.SetActive(false);*/
+			or2Text.SetActive(false);
+			orr2Text.SetActive(false);
 		}
 
 		if (stela2bool == true) {
 			b2Text.SetActive(false);
 			s2Text.SetActive(true);
-			/*or2Text.SetActive(false);
-			orr2Text.SetActive(false);*/
+			or2Text.SetActive(false);
+			orr2Text.SetActive(false);
+		}
+
+		if (other2bool == true) {
+			b2Text.SetActive(false);
+			s2Text.SetActive(false);
+			or2Text.SetActive(true);
+			orr2Text.SetActive(false);
+		}
+
+		if (otherr2bool == true) {
+			b2Text.SetActive(false);
+			s2Text.SetActive(false);
+			or2Text.SetActive(false);
+			orr2Text.SetActive(true);
 		}
 	}
 
@@ -76,8 +102,6 @@ public class CharacterLevelSelect : MonoBehaviour {
 		other1bool = false;
 		otherr1bool = false;
 		p1active = true;
-		//player1 = GameObject.Find ("Canvas").GetComponent<GameManager> ().buddy1;
-		Debug.Log("You have clicked the buddy1!");
 	}
 
 	public void stela1()
@@ -87,8 +111,24 @@ public class CharacterLevelSelect : MonoBehaviour {
 		other1bool = false;
 		otherr1bool = false;
 		p1active = true;
-		//player1 = GameObject.Find ("Canvas").GetComponent<GameManager> ().buddy1;
-		Debug.Log("You have clicked the stela1!");
+	}
+
+	public void or1()
+	{
+		buddy1bool = false;
+		stela1bool = false;
+		other1bool = true;
+		otherr1bool = false;
+		p1active = true;
+	}
+
+	public void orr1()
+	{
+		buddy1bool = false;
+		stela1bool = false;
+		other1bool = false;
+		otherr1bool = true;
+		p1active = true;
 	}
 
 	public void buddy2()
@@ -98,8 +138,6 @@ public class CharacterLevelSelect : MonoBehaviour {
 		other2bool = false;
 		otherr2bool = false;
 		p2active = true;
-		//player1 = GameObject.Find ("Canvas").GetComponent<GameManager> ().buddy1;
-		Debug.Log("You have clicked the buddy2!");
 	}
 
 	public void stela2()
@@ -109,8 +147,24 @@ public class CharacterLevelSelect : MonoBehaviour {
 		other2bool = false;
 		otherr2bool = false;
 		p2active = true;
-		//player1 = GameObject.Find ("Canvas").GetComponent<GameManager> ().buddy1;
-		Debug.Log("You have clicked the stela2!");
+	}
+
+	public void or2()
+	{
+		buddy2bool = false;
+		stela2bool = false;
+		other2bool = true;
+		otherr2bool = false;
+		p2active = true;
+	}
+
+	public void orr2()
+	{
+		buddy2bool = false;
+		stela2bool = false;
+		other2bool = false;
+		otherr2bool = true;
+		p2active = true;
 	}
 
 }
